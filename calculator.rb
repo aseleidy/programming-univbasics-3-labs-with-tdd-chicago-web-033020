43d1f19d0642107519410fd29f8d6952a22d1790
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-21 01:11:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@ first_number = 80
 second_number = 20
 
 sum = first_number + second_number;
-difference = (second_number - first_number)
+difference = second_number - first_number
 product = first_number * second_number
 quotient = second_number/first_number
