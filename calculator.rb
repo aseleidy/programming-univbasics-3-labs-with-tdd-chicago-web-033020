d54a3bf2a67b964dca83a122742fedcdbca9260a
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-21 01:11:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@ first_number = 80
 second_number = 20
 
 sum = first_number + second_number;
-difference = second_number - first_number
+difference = first_number - second_number
 product = first_number * second_number
-quotient = second_number/first_number
+quotient = first_number/second_number
