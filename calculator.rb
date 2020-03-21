6f9993144ebbbf38ce060b04604c2de46912e98e
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-21 01:04:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@
 first_number = rand(100)
 second_number = rand(0...100)
 
-sum = first_number + second_number
+sum = first_number+second_number
 difference = second_number - first_number
 product = first_number * second_number
 quotient = second_number/first_number
